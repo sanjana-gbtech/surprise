275f2fa208880b73ed9ee7e7c8a990792b575d4e
sanjana-gbtech <sanjana.konda@gbtech.in>
2025-02-03 17:20:18 +0530

    first commit

--- src/pages/Envolope.jsx
+++ src/pages/Envolope.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import envlope from '../images.jpg';
 function Envolope(props) {
     return (
-        <div className='d-flex justify-content-center align-items-center' >
+        <div className='d-flex justify-content-center align-items-center ' style={{height:'100vh'}}>
             <img src={envlope} alt="image" className='w-25'   />
         </div>
     );
